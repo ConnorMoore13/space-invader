7063ad93e87746126ac7bc1682e397ca8b330534
ConnorMoore13 <39624268+ConnorMoore13@users.noreply.github.com>
2018-05-25 08:36:45 -0500

    Create js2.js
    average length of words

---
+++ js2.js
@@ -0,0 +1,10 @@
+
+
+function computeAverageLengthOfWords(word1, word2) {
+ 
+  var words = (word1.length + word2.length) / 2;
+    return words;
+
+  }
+var output = computeAverageLengthOfWords('code', 'programs');
+  console.log(output);
