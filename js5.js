77b82ba0356c0e5ecfeb1c4472f8289ef4691721
ConnorMoore13 <39624268+ConnorMoore13@users.noreply.github.com>
2018-05-25 08:38:47 -0500

    Create js5.js
     findMinLengthOfThreeWords

---
+++ js5.js
@@ -0,0 +1,17 @@
+function findMinLengthOfThreeWords(word1, word2, word3) {
+  if (word1.length < word2.length) {
+   if (word1.length < word3.length) {
+      return word1.length;
+   }}
+  else {
+      return word2.length;
+  
+  
+}}
+
+var output = findMinLengthOfThreeWords('a', 'be', 'see');
+console.log(output);
+
+
+  
+  
