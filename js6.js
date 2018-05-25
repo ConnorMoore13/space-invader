6d79aeaf9fb7f0a9fb456d0d687bb4abd003b1e5
ConnorMoore13 <39624268+ConnorMoore13@users.noreply.github.com>
2018-05-25 08:39:23 -0500

    Create js6.js
    computeSummationToN

---
+++ js6.js
@@ -0,0 +1,12 @@
+function computeSummationToN(n) {
+  
+var computedTotal = 0;
+
+  for(var i = 1; i < n + 1; i++){
+    
+    computedTotal += i;}
+    
+  return computedTotal;}
+
+computeSummationToN(6);
+
